920d3db6a23ef27ac970d7d3102c6010d9449e41
Jeongwook Oh <hiphophammer@hotmail.com>
2023-02-16 10:29:43 -0800

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -25,41 +25,55 @@ FROM Production.Product
 WHERE ProductSubcategoryID IS NULL
 
 --5.      Write a query to list the sum of products quantity in the Production.ProductInventory table.
+SELECT SUM(Quantity)
+FROM Production.ProductInventory
 
 --6.    Write a query to list the sum of products in the Production.ProductInventory table and LocationID set to 40 and limit the result to include just summarized quantities less than 100.
-
 --              ProductID    TheSum
-
 --              -----------        ----------
+SELECT ProductID, SUM(Quantity) AS TheSum
+FROM Production.ProductInventory
+WHERE LocationID = 40
+GROUP BY ProductID
+HAVING SUM(Quantity) < 100
 
 --7.    Write a query to list the sum of products with the shelf information in the Production.ProductInventory table and LocationID set to 40 and limit the result to include just summarized quantities less than 100
-
 --    Shelf      ProductID    TheSum
-
 --    ----------   -----------        -----------
+SELECT Shelf, ProductID, SUM(Quantity) AS TheSum
+FROM Production.ProductInventory
+WHERE LocationID = 40
+GROUP BY ProductID, Shelf
+HAVING SUM(Quantity) < 100
 
 --8. Write the query to list the average quantity for products where column LocationID has the value of 10 from the table Production.ProductInventory table.
+SELECT ProductID, AVG(Quantity) AS TheAverage
+FROM Production.ProductInventory
+WHERE LocationID = 10
+GROUP BY ProductID
 
 --9.    Write query  to see the average quantity  of  products by shelf  from the table Production.ProductInventory
-
 --    ProductID   Shelf      TheAvg
-
 --    ----------- ---------- -----------
+SELECT ProductID, Shelf, AVG(Quantity) AS TheAverage
+FROM Production.ProductInventory
+WHERE LocationID = 10
+GROUP BY ProductID, Shelf
 
 --10.  Write query  to see the average quantity  of  products by shelf excluding rows that has the value of N/A in the column Shelf from the table Production.ProductInventory
-
 --    ProductID   Shelf      TheAvg
-
 --    ----------- ---------- -----------
+SELECT ProductID, Shelf, AVG(Quantity) AS TheAverage
+FROM Production.ProductInventory
+WHERE LocationID = 10
+GROUP BY ProductID, Shelf
+HAVING Shelf != 'N/A'
 
 --11.  List the members (rows) and average list price in the Production.Product table. This should be grouped independently over the Color and the Class column. Exclude the rows where Color or Class are null.
-
 --    Color                        Class              TheCount          AvgPrice
-
 --    -------------- - -----    -----------            ---------------------
 
 --Joins:
-
 --12.   Write a query that lists the country and province names from person. CountryRegion and person. StateProvince tables. Join them and produce a result set similar to the following.
 
 --    Country                        Province
@@ -107,11 +121,7 @@ WHERE ProductSubcategoryID IS NULL
 --26.  Display all the Managers who have more than 2 employees reporting to them.
 
 --27.  Display the customers and suppliers by city. The results should have the following columns
-
 --City
-
 --Name
-
 --Contact Name,
-
 --Type (Customer or Supplier)
